1545750d50ef70c75a74215b2c5efbbab7a6572c
金炳 <hzjinbing@corp.netease.com>
2018-07-01 23:36:08 +0800

    change: 修改了存储配置详情这块的逻辑

--- dist/demo/default/src/app/theme/pages/default/config-manage/config-manage.component.ts
+++ dist/demo/default/src/app/theme/pages/default/config-manage/config-manage.component.ts
@@ -8,11 +8,17 @@ import {Ajax} from '../../../../shared/ajax/ajax.service';
 export class ConfigManageComponent implements OnInit {
     productList: any[] = [];
     selectProductId: any = null;
+    selectProductInfo: any = null;
     envs: any[] = [];
     labels: any[] = [];
     selectEnvId: any = null;
+    selectEnvInfo: any = null;
     selectLabelId: any = null;
+    selectLabelInfo: any = null;
     envParamsTemList: any[] = [];
+    encryptKeyList: any[] = [];
+    persistentList: any[] = [];
+    persistent: any[] = [];
     constructor(private ajax: Ajax) {}
 
     ngOnInit(): void {
@@ -26,25 +32,29 @@ export class ConfigManageComponent implements OnInit {
             this.selectProductId =
                 this.productList.length > 0 ? this.productList[0].id : null;
         }
-        await this.getEnvParamsTemplate();
+        this.selectProduct(this.selectProductId);
     }
 
     async selectProduct(id) {
         this.selectProductId = id;
-        let selectProduct = this.productList.filter(item => {
+        this.selectProductInfo = this.productList.filter(item => {
             if (this.selectProductId == item.id) {
                 return true;
             }
         })[0];
-        this.envs = selectProduct.envs;
+        this.envs = this.selectProductInfo.envs;
         if (this.envs.length > 0) {
             this.selectEnvId = this.envs[0].id;
+            this.selectEnvInfo = this.envs[0];
         }
-        this.labels = selectProduct.labels;
+        this.labels = this.selectProductInfo.labels;
         if (this.labels.length > 0) {
             this.selectLabelId = this.labels[0].id;
+            this.selectLabelInfo = this.labels[0];
         }
         await this.getEnvParamsTemplate();
+        await this.getEncrptKeyList();
+        await this.getPersistentList();
     }
 
     /**
@@ -59,12 +69,23 @@ export class ConfigManageComponent implements OnInit {
     }
 
     /**
+     * 加密Key清单
+     * @author stone-jin, https://www.520stone.com
+     */
+    async getEncrptKeyList() {
+        let result = await this.ajax.get('/xhr/encryptKey', {});
+        this.encryptKeyList = result;
+    }
+
+    /**
      * 选中对应的版本
      * @param id 版本的id
      * @author stone-jin, https://www.520stone.com
      */
-    selectLabel(id) {
+    async selectLabel(id) {
         this.selectLabelId = id;
+        await this.getEnvParamsTemplate();
+        await this.getEncrptKeyList();
     }
 
     /**
@@ -72,7 +93,28 @@ export class ConfigManageComponent implements OnInit {
      * @param id 环境的id
      * @author stone-jin, https://www.520stone.com
      */
-    selectEnv(id) {
+    async selectEnv(id) {
         this.selectEnvId = id;
+        await this.getEnvParamsTemplate();
+    }
+
+    /**
+     * 根据当前项目名称，环境名称，版本名称获取当前的存储配置信息
+     */
+    async getPersistentList() {
+        let result = await this.ajax.get('/xhr/property/persistent', {
+            project: this.selectProductInfo.name,
+            profile: this.selectEnvInfo.name,
+            label: this.selectLabelInfo.name,
+        });
+        this.persistentList = result;
+        this.persistent = [];
+        let keys = Object.keys(this.persistentList);
+        for (let i = 0; i < keys.length; i++) {
+            this.persistent.push({
+                key: keys[i],
+                value: this.persistentList[keys[i]],
+            });
+        }
     }
 }
